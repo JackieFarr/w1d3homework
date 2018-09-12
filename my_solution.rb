2504a3f1f989607edf1937cb34c4dc8778261edd
Jackie Farr <jacquelynfarr@hotmail.com>
2018-09-12 17:57:57 +0100

    added Northern Ireland

--- my_solution.rb
+++ my_solution.rb
@@ -163,10 +163,17 @@ united_kingdom = [
 ]
 # Complete these tasks:
 # Change the capital of Wales from "Swansea" to "Cardiff".
-p united_kingdom[1][:capital] = "cardiff"
-p united_kingdom
+# p united_kingdom[1][:capital] = "cardiff"
+# p united_kingdom
 
 
 # Create a Hash for Northern Ireland and add it to the united_kingdom array (The capital is Belfast, and the population is 1,811,000).
+
+united_kingdom.push(name: "Belfast", population: 1811000)
+p united_kingdom
+
+
+
+
 # Use a loop to print the names of all the countries in the UK.
 # Use a loop to find the total population of the UK.
